f13dd5459cb4e365178deb394c04d9ab492a05f3
jefferson nwadi <111717472+jeff-nwadi@users.noreply.github.com>
2026-01-13 00:32:57 +0100

    commit

--- plotline/components/ContentPage.tsx
+++ plotline/components/ContentPage.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import Logos from '../images/Frame 124.svg'
+import TriangleUp from '../images/image 170.svg'
+import TriangleDown from '../images/image 169.svg'
 
 export const ContentPage = () => {
   return (
@@ -10,9 +12,12 @@ export const ContentPage = () => {
       </div>
      
      {/* mobile screen */}
-     <div className='flex md:hidden -mt-20'>
-      
+     <div className='flex md:hidden -mt-20 relative'>    
+      <Image src={TriangleDown} alt="" className='absolute -top-12 w-18 h-18' />
+      <div className='px-5'>
         <video src="/storytribe-demo.mp4" loop autoPlay muted className='w-full h-auto rounded-4xl' />
+      </div>
+       <Image src={TriangleUp} alt="" className='absolute -bottom-12 right-0 w-18 h-18' />
      </div>
     </div>
   )
